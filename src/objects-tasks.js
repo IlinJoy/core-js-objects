74a4e4c4eba05f1e273f849202c43323a80492fa
Mariya Ilina <m.polibit@gmail.com>
2025-03-01 12:25:00 +0300

    feat: emplement removeProperties

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -50,8 +50,14 @@ function mergeObjects(/* objects */) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, ['age']) => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+function removeProperties(obj, keys) {
+  const newObj = { ...obj };
+  keys.forEach((key) => {
+    if (key in obj) {
+      delete newObj[key];
+    }
+  });
+  return newObj;
 }
 
 /**
